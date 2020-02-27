9acac7edd3ca148923690bda960408fe984b8f7a
Amy Ung <amyung.lm@gmail.com>
2020-02-27 15:44:52 -0800

    Fixing Jquery to connect with HTML and index.js

--- Eventonica/jQuery-scripts.js
+++ Eventonica/jQuery-scripts.js
@@ -1,12 +1,32 @@
 $(document).ready(() => {
     const eventRecommender = new EventRecommender();
+    // eventRecommender.addUser('liz Audry');
+    // eventRecommender.addUser('20thCentDaddy', 'joseph');
+    // eventRecommender.addUser('XcrusheddreamsX', 'caesar');
+    // eventRecommender.addUser('GhostPuncher', 'star platinum');
+    // eventRecommender.addUser('steelballz', 'gyro', '94602');
+    // eventRecommender.addEvent('00001', 'the party', 'festival', 'San Francisco, CA', 'golden gate park', 'a real fuckin party');
+    // eventRecommender.addEvent('00002', 'the blood brothers', 'concert', 'Oakland, CA', 'The Fox', 'the blood brothers, featuring celebration');
+    // eventRecommender.addEvent('00003', 'rilo kiley', 'concert', 'Oakland, CA', 'The New Parkway', 'reunited and it feels so good', '19:00');
+    // eventRecommender.addEvent('00004', '...and you will know us by the trail of dead', 'concert', 'San Francisco, CA', 'Slim\'s', 'they\'re about to rock your socks off');
+    // eventRecommender.addEvent('00005', 'San Francisco Food and Wine Festival', 'culture', 'San Francisco, CA', 'Fort Mason Center', 'come enjoy some food and wine');
+    // eventRecommender.addEvent('00006', 'against me!', 'concert', 'San Francisco, CA', 'Great American Music Hall', 'hell yeah, that\'s them. that\'s against me!!');
+    
+    // eventRecommender.addUser('Amy Ung');
     
     //Adding new user
-    $("#add-user").submit(() => {
-        var str = $("#add-user-name").val().toUpperCase();
-        var num = $("#add-user-id").val();
+    $("#add-user").submit(function() {
+        let str = $("#add-user-name").val().toUpperCase();
+        let num = $("#add-user-id").val();
+        console.log(str,num);
+
+        eventRecommender.addUser(num,str);
         if(str == '' && num == ''){
             alert("Error! Missing name or id nubmer.");
+            return false;
+        }
+        else if( num.type() == number){
+            alert("Error! Not name or id nubmer.");
             return false;
         }
         else{
@@ -18,17 +38,20 @@ $(document).ready(() => {
 
     //Deleting user
     $("#delete-user").submit(() => {
-        var num = $("#delete-user-id").val();
+        let num = $("#delete-user-id").val();
+        eventRecommender.deleteUser(num);
         if(num == ''){
             alert("Error! Missing id nubmer.");
             return false;
         }
+    
     })
 
     //Adding new event
     $("#add-event").submit(() => {
-        var str = $("#add-event-name").val().toUpperCase();
-        var num = $("#add-event-id").val();
+        let str = $("#add-event-name").val().toUpperCase();
+        let num = $("#add-event-id").val();
+        eventRecommender.addEvent(num,str);
         if(str == '' && num == ''){
             alert("Error! Missing name or id nubmer.");
             return false;
@@ -37,14 +60,18 @@ $(document).ready(() => {
         return false;
         }
     });
-
     //Deleting event
     $("#delete-event").submit(() => {
-        var num = $("#delete-event-id").val();
+        let num = $("#delete-event-id").val();
+        eventRecommender.deleteEvents(num);
         if(num == ''){
             alert("Error! Missing id nubmer.");
             return false;
         }
+    })
+
+    $("#keyword-search").submit(() => {
+        let keyS = $("#keyword-search-id").val();
     })
 
 });
